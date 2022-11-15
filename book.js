9164595d9ac9802f1293d039b92207d77834d27e
Ridwan Salim <ridwan.salim@gmail.com>
2022-11-15 21:45:31 +0600

    Added

--- book.js
+++ book.js
@@ -1,6 +1,15 @@
 const searchBook = () => {
     const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
-    console.log(searchText);
+    // console.log(searchText);
     searchField.value = '';
+
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data))
+}
+
+const displaySearchResult = books => {
+    
 }
