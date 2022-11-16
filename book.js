d2d50341f77c6e8dd777bcf4fc123b2693ea27e5
Ridwan Salim <ridwan.salim@gmail.com>
2022-11-16 11:23:19 +0600

    Added

--- book.js
+++ book.js
@@ -20,7 +20,15 @@ const displaySearchResult = data => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-    
+    <div class="card" style="width: 18rem;">
+        <img src="..." height="300px" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title"><b>Book Name:</b><i></i></h5>
+          <p class="card-text"><b>Author Name:</b><i></i></p>
+          <p class="card-text"><b>Book Publisher:</b><i></i></p>
+          <p class="card-text"><b>Published Date:</b><i></i></p>
+        </div>
+    </div>
         `;
         searchResult.appendChild(div);
     })
